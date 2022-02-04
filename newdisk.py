c2e3d79ecd9027989008e4f5edc9363d56cbdd4a
paavampayyan <81892911+paavampayyan@users.noreply.github.com>
2022-02-04 10:50:26 +0530

    Update newdisk.py

--- newdisk.py
+++ newdisk.py
@@ -22,7 +22,7 @@ kutty = Client(ss,
 
 def starting():
     for vid in kutty.search_messages(-483088952, filter='video', limit=1):
-        bv = kutty.copy_message('@mdisk7bot', from_chat_id= -1001707716901, message_id=vid.message_id)
+        bv = kutty.copy_message('@mdisk7bot', from_chat_id= -483088952, message_id=vid.message_id)
         vid.download(file_name=f'res/{bv.message_id}.mp4')
         vid.delete()
         get_thump(f'res/{bv.message_id}.mp4', f'{bv.message_id}.png')

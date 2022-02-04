0a121d0af30102c598704cd9904a2d6d2bfa193f
paavampayyan <81892911+paavampayyan@users.noreply.github.com>
2022-02-04 10:42:29 +0530

    ok

--- newdisk.py
+++ newdisk.py
@@ -21,14 +21,17 @@ kutty = Client(ss,
 
 
 def starting():
-    for vid in kutty.search_messages(-483088952, filter='video', limit=1):
-        bv = kutty.copy_message('@mdisk1bot', from_chat_id=-483088952, message_id=vid.message_id)
+    for vid in kutty.search_messages(-1001707716901, filter='video', limit=1):
+        bv = kutty.copy_message('@mdisk7bot', from_chat_id= -1001707716901, message_id=vid.message_id)
         vid.download(file_name=f'res/{bv.message_id}.mp4')
         vid.delete()
         get_thump(f'res/{bv.message_id}.mp4', f'{bv.message_id}.png')
         gen_cover(f'{bv.message_id}.png', bv.message_id)
         time.sleep(1)
-        g=kutty.send_message('@mdisk1bot', text=vid.caption)
+        if vid.caption == None:
+            g = kutty.send_message('@mdisk7bot', text=f'new hot video')
+        else:
+            g = kutty.send_message('@mdisk7bot', text=vid.caption)
         print(g.message_id)
     else:   
         print('Ok............')
@@ -39,32 +42,30 @@ def starting():
 def get_id(__, m:Messages):
     m.edit(f'chat id is  `{m.chat.id}`')   
 
-@kutty.on_message(filters.regex(r'https://mdisk.me/') & filters.user('@mdisk1bot'))
+@kutty.on_message(filters.regex(r'https://mdisk.me/') & filters.user('@mdisk7bot'))
 def get_link(__, m:Messages):
     print(m)
     time.sleep(1)
-    file_caption = kutty.get_messages(chat_id='@mdisk1bot', message_ids=m.reply_to_message.message_id + 2)
-    print(file_caption.text)
-    c1 = kutty.send_photo(-1001595060068, photo=f'final{m.reply_to_message.message_id}.png',
-                    caption=f'**TAG : {file_caption.text}**'
-                            f'\n\n➖➖➖➖➖➖➖➖➖➖➖➖'
-                            f'\n**{m.text}**'
-                            f'\n**{m.text}**\n'
-                            f'➖➖➖➖➖➖➖➖➖➖➖➖'
-                            f'\n\n**Watch On MX PLayer 🔷**')
-    c1.copy(-1001626342573)
-    b1 = c1.copy('pdisk_link_adminbot')
-    kutty.send_message('pdisk_link_adminbot', text='/broadcast', reply_to_message_id=b1.message_id)
+    file_caption = m.reply_to_message.caption
+    if file_caption == None:
+        file_caption = 'Top video Today 😍'
+    c1 = kutty.send_photo('@AD_Mdisk_Bot', photo=f'final{m.reply_to_message.message_id}.png',
+                    caption=f'**TAG : {file_caption}**'
+                            f'\n**{m.text}**')
+    # c1.copy(-1001626342573)
+    # b1 = c1.copy('pdisk_link_adminbot')
+    # kutty.send_message('pdisk_link_adminbot', text='/broadcast', reply_to_message_id=b1.message_id)
     time.sleep(1)
     os.remove(f'final{m.reply_to_message.message_id}.png')
     os.remove(f'{m.reply_to_message.message_id}.png')
     os.remove(f'res/{m.reply_to_message.message_id}.mp4')
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(starting, 'interval' , minutes=10)
+scheduler.add_job(starting, 'interval' , seconds=60)
 
 
 scheduler.start()
+
 
 
 
